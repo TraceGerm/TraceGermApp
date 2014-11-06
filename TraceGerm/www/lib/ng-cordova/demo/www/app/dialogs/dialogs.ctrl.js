0d4ec4adf24ab233d0027d63153c3f700eeda13b
askos92 <askoanti@gmail.com>
2014-11-06 20:03:58 +0200

    rename of the project

---
+++ TraceGerm/www/lib/ng-cordova/demo/www/app/dialogs/dialogs.ctrl.js
@@ -0,0 +1,56 @@
+angular.module('demo.dialogs.ctrl', [])
+
+  .controller('DialogsCtrl', function ($scope, $cordovaDialogs, $ionicModal) {
+
+    $scope.action = "Press any button";
+
+    $scope.alert = function () {
+      $scope.action = "Alert";
+      $cordovaDialogs.alert('Wow!');
+    };
+
+    $scope.confirm = function () {
+      $scope.action = "Confirm";
+      $cordovaDialogs.confirm('Are you sure?', "Custom title").then(function (buttonIndex) {
+        $cordovaDialogs.alert("Button index : " + buttonIndex);
+      });
+    };
+
+    $scope.prompt = function () {
+      $scope.action = "Prompt";
+      $cordovaDialogs.prompt('Please Login', "Custom title").then(function (result) {
+       $cordovaDialogs.alert("Input: " + result.input1 + "\n Button index : " + result.buttonIndex);
+      });
+    };
+
+    $scope.beep = function () {
+      $scope.action = "Beep";
+      $cordovaDialogs.beep(3);
+    };
+
+
+    /*
+     Ionic modal with source code
+     */
+
+    $ionicModal.fromTemplateUrl('app/dialogs/dialogs-source.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.closeModal = function () {
+      $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function () {
+      $scope.modal.remove();
+    });
+
+    $scope.showSource = function () {
+      $scope.modal.show();
+    }
+
+  });
+
---
+++ TraceGerm/www/lib/ng-cordova/gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp'),
+  buildConfig = require('./config/build.config'),
+  concat = require('gulp-concat'),
+  argv = require('minimist')(process.argv.slice(2)),
+  footer = require('gulp-footer'),
+  header = require('gulp-header'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  karma = require('karma').server,
+  karmaConf = require('./config/karma.conf.js'),
+  rename = require('gulp-rename'),
+  shell = require('gulp-shell');
+
+
+gulp.task('default', ['build']);
+
+gulp.task('build', function () {
+  gulp.src(buildConfig.mockFiles)
+    .pipe(concat('ng-cordova-mocks.js'))
+    .pipe(header(buildConfig.closureStart))
+    .pipe(footer(buildConfig.closureEnd))
+    .pipe(header(buildConfig.banner))
+    .pipe(gulp.dest(buildConfig.dist))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest(buildConfig.dist));
+
+  return gulp.src(buildConfig.pluginFiles)
+    .pipe(concat('ng-cordova.js'))
+    .pipe(header(buildConfig.closureStart))
+    .pipe(footer(buildConfig.closureEnd))
+    .pipe(header(buildConfig.banner))
+    .pipe(gulp.dest(buildConfig.dist))
+    .pipe(gulp.dest(buildConfig.demo.ngCordova))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest(buildConfig.dist))
+    .pipe(gulp.dest(buildConfig.demo.ngCordova));
+});
+
+gulp.task('karma', function (done) {
+  karmaConf.singleRun = true;
+  argv.browsers && (karmaConf.browsers = argv.browsers.trim().split(','));
+  argv.reporters && (karmaConf.reporters = argv.reporters.trim().split(','));
+  karma.start(karmaConf, done);
+});
+
+gulp.task('lint', function () {
+  return gulp.src('./src/plugins/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('karma-watch', function (done) {
+  console.log(karmaConf);
+  karmaConf.singleRun = false;
+  karma.start(karmaConf, done);
+});
+
+gulp.task('watch', ['build'], function () {
+  gulp.watch(['src/**/*.js', 'test/**/*.js'], ['build']);
+});
+
+
+gulp.task('run-demo', ['watch'], shell.task([
+  'cd demo &&  ionic run ios -l -c --target="iPhone (Retina 4-inch)"'
+]));
